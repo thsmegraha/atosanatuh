2dde9d253a5983e2eed1157b07ec9b13d72ef64e
thsmegraha <148853931+thsmegraha@users.noreply.github.com>
2024-10-07 14:24:26 +0700

    Update NonceFinder.js

--- includes/NonceFinder.js
+++ includes/NonceFinder.js
@@ -3,7 +3,7 @@ import { spawn, Thread, Worker } from "threads";
 export default class NonceFinder {
     constructor(params = {}) {
         this._workers = [];
-        this._noncesPerRun = 5000000;
+        this._noncesPerRun = 15000000;
         this._tryingNonces = [];
         this._initialNonce = 0;
         this._startedSearchAt = null;
